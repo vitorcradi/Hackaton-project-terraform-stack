24552c7e95028c217a522f0ce4dc825155d8a8ae
Ubuntu <ubuntu@ip-172-31-80-10.ec2.internal>
2020-03-28 19:02:57 +0000

    group name update

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackaton-fiap-1dvp-334034"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
